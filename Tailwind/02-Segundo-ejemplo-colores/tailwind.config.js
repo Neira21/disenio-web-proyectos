53035ccc5d88fe178d621a3aa54c92912564fa72
Alvaro Neira <alvaroneira98@gmail.com>
2023-11-22 20:32:35 -0500

    tailwind colores

---
+++ Tailwind/02-Segundo-ejemplo-colores/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'azul-claro': '#00B4D8',
+        'azul-oscuro': '#03045E',
+      }
+    },
+  },
+  plugins: [],
+}
+
